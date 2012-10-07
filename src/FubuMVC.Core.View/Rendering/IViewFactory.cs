e1a2d2080dc57aba0a9d7eb93a4d0c4ee3adf109
jeremydmiller <jeremydmiller@yahoo.com>
2012-10-07 15:56:35 -0500

    initial import of the code from FubuMVC

---
+++ src/FubuMVC.Core.View/Rendering/IViewFactory.cs
@@ -0,0 +1,10 @@
+﻿using FubuCore.Descriptions;
+
+namespace FubuMVC.Core.View.Rendering
+{
+    public interface IViewFactory : DescribesItself
+    {
+        IRenderableView GetView();
+        IRenderableView GetPartialView();
+    }
+}
